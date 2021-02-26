a3155f29a6be6627deb9bbcbe41a432584c437e8
Cherish0207 <cheshire_1207@foxmail.com>
2021-02-26 10:18:32 +0800

    登陆接口:服务器存入用户唯一表示cookie

--- server.js
+++ server.js
@@ -12,15 +12,22 @@ let userList = [
   { username: "cherish", password: "1234", money: 10000 },
   { username: "silence", password: "5678", money: 20 },
 ];
+let SESSION_ID = "connect.sid";
+let session = {};
 app.post("/api/login", function (req, res) {
   let { username, password } = req.body;
   let user = userList.find(
     (user) => user.username === username && user.password === password
   );
   if (user) {
+    // 服务器需要在用户登录后 给一个信息  cookie 珠峰:110
+    let cardId = Math.random() + Date.now();
+    session[cardId] = { user };
+    console.log(cardId);
+    res.cookie(SESSION_ID, cardId);
     res.json({ code: 0 });
   } else {
     res.json({ code: 1, error: "用户不存在" });
   }
 });
-app.listen(3001);
+app.listen(3000);
