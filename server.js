4e66c17cfbf0a1b74f4ffc89e80877320ace21bd
Cherish0207 <cheshire_1207@foxmail.com>
2021-02-26 10:10:27 +0800

    登陆判断;使用body-parser

--- server.js
+++ server.js
@@ -6,4 +6,21 @@ let path = require("path"); // 帮我们拼接路径
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname)));
 
+let bodyParser = require("body-parser"); // 处理浏览器发过来的请求体
+app.use(bodyParser.urlencoded({ extended: true })); // a=1&b=2 = {a:1,b:2} =  req.body
+let userList = [
+  { username: "cherish", password: "1234", money: 10000 },
+  { username: "silence", password: "5678", money: 20 },
+];
+app.post("/api/login", function (req, res) {
+  let { username, password } = req.body;
+  let user = userList.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (user) {
+    res.json({ code: 0 });
+  } else {
+    res.json({ code: 1, error: "用户不存在" });
+  }
+});
 app.listen(3001);
